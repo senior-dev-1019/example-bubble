95c2406155899fdd47ce7321ccc5edbc01d23ec7
Senior Dev <seniordeveloper1019@gmail.com>
2022-09-01 16:50:34 +0200

    upload for checking

---
+++ example-bubble.php
@@ -0,0 +1,28 @@
+<?php
+/*
+Plugin Name: Example bubble
+Description: Example bubble can easily add bubble ornament in your post, without direct HTML&CSS coding. 
+Version: 1.0.3
+Author: Senior Dev
+License: GPLv2 or later
+*/
+
+
+/*
+Copyright (C) 2014 Senior Dev
+
+This program is free software; you can redistribute it and/or
+modify it under the terms of the GNU General Public License
+as published by the Free Software Foundation; either version 2
+of the License, or (at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+*/
+
+require_once dirname( __FILE__ ) . '/classes/SnbBubble.php';
+
+new SnbBubble();
---
+++ readme.txt
@@ -0,0 +1,25 @@
+=== Example Bubble
+Contributors: Senior Dev
+Tags: Bubble Posts
+Requires at least: 3.7.1
+Tested up to: 3.7.1
+Stable tag: 1.0.3
+License: GPLv2 or later
+License URI: http://www.gnu.org/licenses/gpl-2.0.html
+
+Easy to add bubbles in your post.
+
+
+== Description ==
+You can easily add bubble ornament in your post, without direct HTML&CSS coding.
+
+<h3>Features</h3>
+<ul>
+<li>Ornament your posts with bubbles (like Chat,Act,lect.,etc)</li>
+<li>9 pattern bubbles are available (at version 1.0)</li>
+<li>Fit narrow frame automatically</li>
+<li>By short code, use bubble.</li>
+<li>Quickly use them without using direct HTML&CSS code and database</li>
+<li>In a post, you can use same speach bubble more easily (Preset Type)</li>
+<li>Not support IE8</li>
+</ul>
